89242505ba17a2f98d21478d03ea55a0a07252dd
MulagadaVamsi <147685873+MulagadaVamsi@users.noreply.github.com>
2026-06-19 18:18:27 +0530

    Add files via upload

---
+++ listing.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+
+const listingSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+    },
+    image:{
+        url: String,
+    },
+    price:{
+        type:Number,
+    },
+    location:{
+        type:String,
+    },
+    country:{
+        type:String,
+    }
+})
+
+const listing=new mongoose.model("listing",listingSchema);
+
+module.exports=listing;
